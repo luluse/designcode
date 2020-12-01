6e5e8db2798bb5f5e7fcfc1d124cee564bf839ea
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2020-12-01 11:45:22 -0700

    added safeAreaView and scrollView

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text } from 'react-native';
+import { ScrollView, SafeAreaView } from 'react-native';
 import styled from 'styled-components';
 import Card from './components/Card';
 
 export default function App() {
   return (
     <Container>
-      <TitleBar>
-        <Avatar source={require('./assets/avatar.jpg')}/>
-        <Title>Welcome to React native</Title>
-        <Name>Lulu</Name>
-      </TitleBar>
-      <Subtitle>Continue learning</Subtitle>
-      <Card />
+      <SafeAreaView>
+        <ScrollView style={{ height: '100%' }}>
+          <TitleBar>
+            <Avatar source={require('./assets/avatar.jpg')} />
+            <Title>Welcome to React native</Title>
+            <Name>Lulu</Name>
+          </TitleBar>
+          <Subtitle>Continue learning</Subtitle>
+          <ScrollView horizontal={true} style={{ paddingBottom: 30 }} showsHorizontalScrollIndicator={false}>
+            <Card
+              title='Styled Components'
+              image={require('./assets/background2.jpg')}
+              caption='Ract Native'
+              logo={require('./assets/logo-react.png')}
+              subtitle='5 of 12 sections'
+            />
+            <Card
+              title='Styled Components 2'
+              image={require('./assets/background1.jpg')}
+              caption='Ract Native'
+              logo={require('./assets/logo-react.png')}
+              subtitle='5 of 12 sections'
+            />
+          </ScrollView>
+        </ScrollView>
+      </SafeAreaView>
     </Container>
   );
 }
