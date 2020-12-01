2a59b62d0113461618f1839e8c8e36b46f5171ea
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2020-12-01 12:18:53 -0700

    turned svg icon into jsx

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import React from 'react';
 import { ScrollView, SafeAreaView } from 'react-native';
 import styled from 'styled-components';
 import Card from './components/Card';
-import { Ionicons } from '@expo/vector-icons';
+// import { Ionicons } from '@expo/vector-icons';
+import { NotificationIcon } from './components/Icons';
 
 export default function App() {
   return (
@@ -14,7 +15,7 @@ export default function App() {
             <Avatar source={require('./assets/avatar.jpg')} />
             <Title>Welcome to React native</Title>
             <Name>Lulu</Name>
-            <Ionicons name="ios-notifications" size={32} color="#4775f2" style={{ position: "absolute", right: 20, top: 5 }} />
+            <NotificationIcon style={{ position: "absolute", right: 20, top: 5 }} />
 
           </TitleBar>
           <Subtitle>Continue learning</Subtitle>
