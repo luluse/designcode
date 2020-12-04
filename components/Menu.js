02373a55b797ed6bb04419ce40344ba6317802c8
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2020-12-04 11:13:17 -0700

    finished profile content

--- components/Menu.js
+++ components/Menu.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Animated, TouchableOpacity, Dimensions } from 'react-native';
 import { Ionicons } from "@expo/vector-icons";
+import MenuItem from './MenuItems';
 
 
 const screenHeight = Dimensions.get('window').height;
@@ -26,12 +27,26 @@ class Menu extends React.Component {
   render() {
     return (
       <AnimatedContainer style={{ top: this.state.top }}>
-        <Cover />
-        <TouchableOpacity onPress={this.toggleMenu} style={{ position: 'absolute', top: 120, left: '50%', marginLeft: -22, zIndex: 1}}>
+        <Cover>
+          <Image source={require('../assets/background2.jpg')} />
+          <Title>Lulu</Title>
+          <Subtitle>lulu@gmail.com</Subtitle>
+
+        </Cover>
+        <TouchableOpacity onPress={this.toggleMenu} style={{ position: 'absolute', top: 120, left: '50%', marginLeft: -22, zIndex: 1 }}>
           <CloseView>
             <Ionicons name="ios-close" size={44} color="#546bfb" /></CloseView>
         </TouchableOpacity>
-        <Content />
+        <Content>
+          {items.map((item, index) => (
+            <MenuItem
+              key={index}
+              icon={item.icon}
+              title={item.title}
+              text={item.text}
+            />
+          ))}
+        </Content>
       </AnimatedContainer>
     )
   }
@@ -52,6 +67,8 @@ const AnimatedContainer = Animated.createAnimatedComponent(Container)
 const Cover = styled.View`
   height: 142px;
   background: black;
+  align-items: center;
+  justify-content: center;
 `;
 
 const CloseView = styled.View`
@@ -67,5 +84,46 @@ const CloseView = styled.View`
 const Content = styled.View`
   height: ${screenHeight};
   background: #f0f3f5;
+  padding: 50px;
 `;
 
+const Image = styled.Image`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-size: 24px;
+  font-weight: 600;
+`;
+
+const Subtitle = styled.Text`
+  font-size: 13;
+  color: rgba(255, 255, 255, 0.5);
+  margin-top: 8px;
+`;
+
+const items = [
+  {
+    icon: "ios-settings",
+    title: "Account",
+    text: "settings"
+  },
+  {
+    icon: "ios-card",
+    title: "Billing",
+    text: "payments"
+  },
+  {
+    icon: "ios-compass",
+    title: "Learn React",
+    text: "start course"
+  },
+  {
+    icon: "ios-exit",
+    title: "Log out",
+    text: "see you soon!"
+  }
+]
