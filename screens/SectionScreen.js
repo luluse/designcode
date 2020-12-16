4858d4b53b2369738bc9577a9787832b2151f30f
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2020-12-16 11:06:54 -0700

    added style to section screen and close button

--- screens/SectionScreen.js
+++ screens/SectionScreen.js
@@ -1,24 +1,52 @@
 import React from "react";
 import styled from "styled-components";
-import { Button } from "react-native";
+import { TouchableOpacity, StatusBar } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 class SectionScreen extends React.Component {
   static navigationOptions = {
     header: null
   };
+
+  componentDidMount() {
+    StatusBar.setBarStyle("light-content", true);
+  }
+  
+  componentWillUnmount() {
+    StatusBar.setBarStyle("dark-content", true);
+  }
+
   render() {
     const { navigation } = this.props
     const section = navigation.getParam('section');
 
     return (
       <Container>
-        <Text>{section.title}</Text>
-        <Button
-          title="Close"
+        <StatusBar hidden />
+        <Cover>
+          <Image source={section.image} />
+          <Title>{section.title}</Title>
+          <Caption>{section.caption}</Caption>
+        </Cover>
+        <TouchableOpacity
           onPress={() => {
             this.props.navigation.goBack();
           }}
-        />
+          style={{
+            position: "absolute",
+            top: 20,
+            right: 20
+          }}
+        >
+          <CloseView>
+            <Ionicons
+              name="ios-close"
+              size={36}
+              style={{ marginTop: -2 }}
+              color="#4775f2"
+            />
+          </CloseView>
+        </TouchableOpacity>
       </Container>
     );
   }
@@ -28,8 +56,45 @@ export default SectionScreen;
 
 const Container = styled.View`
   flex: 1;
+`;
+
+const Cover = styled.View`
+  height: 375px;
+`;
+
+const Image = styled.Image`
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  background: #3c4560;
+`;
+
+const Title = styled.Text`
+  font-size: 24px;
+  color: white;
+  font-weight: bold;
+  width: 170px;
+  position: absolute;
+  top: 78px;
+  left: 20px;
+`;
+
+const Caption = styled.Text`
+  color: white;
+  font-size: 17;
+  position: absolute;
+  bottom: 20px;
+  left: 20px;
+  max-width: 300px;
+`;
+
+const CloseView = styled.View`
+  width: 32px;
+  height: 32px;
+  background: white;
+  border-radius: 22px;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.15);
   justify-content: center;
   align-items: center;
 `;
-
-const Text = styled.Text``;
