8be840fe6a032e9dca4a4c959acce7c1af61eb54
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2020-12-16 11:12:20 -0700

    section screen is fully implemented usign static data

--- screens/SectionScreen.js
+++ screens/SectionScreen.js
@@ -11,7 +11,7 @@ class SectionScreen extends React.Component {
   componentDidMount() {
     StatusBar.setBarStyle("light-content", true);
   }
-  
+
   componentWillUnmount() {
     StatusBar.setBarStyle("dark-content", true);
   }
@@ -25,6 +25,12 @@ class SectionScreen extends React.Component {
         <StatusBar hidden />
         <Cover>
           <Image source={section.image} />
+
+          <Wrapper>
+            <Logo source={section.logo} />
+            <Subtitle>{section.subtitle}</Subtitle>
+          </Wrapper>
+
           <Title>{section.title}</Title>
           <Caption>{section.caption}</Caption>
         </Cover>
@@ -97,4 +103,25 @@ const CloseView = styled.View`
   box-shadow: 0 5px 10px rgba(0, 0, 0, 0.15);
   justify-content: center;
   align-items: center;
+`;
+
+const Wrapper = styled.View`
+  flex-direction: row;
+  position: absolute;
+  top: 40px;
+  left: 20px;
+  align-items: center;
+`;
+
+const Logo = styled.Image`
+  width: 24px;
+  height: 24px;
+`;
+
+const Subtitle = styled.Text`
+  font-size: 15px;
+  font-weight: 600;
+  color: rgba(255, 255, 255, 0.8);
+  margin-left: 5px;
+  text-transform: uppercase;
 `;
